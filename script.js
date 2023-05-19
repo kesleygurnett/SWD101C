916bc2e5f05293e690ca9b355384fcb7616cd98a
kesleygurnett <109889319+kesleygurnett@users.noreply.github.com>
2023-05-19 11:12:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function numberChecking(number1: 432, number2: 10){
-    if(number1 > 100){
-        console.log("Number 1 has many digits!");
+class Employee {
+    constructor(name, salary, hireDate) {
+      this.name = name;
+      this.salary = salary;
+      this.hireDate = hireDate;
     }
-    if(number1 < 0){
-        console.log("Number 1 is a negative number!");
+    getName() {
+      console.log(this.name.toUpperCase());
     }
-    if(number2 < 20){
-        console.log("Number 2 is not a very high number.");
+    getSalary() {
+      console.log(this.salary);
     }
-    if(number2 > 50){
-        console.log("Number 2 is a high number.");
+    getHireDate() {
+      console.log(this.hireDate);
     }
-}
-
-numberChecking(432, 10);
+  }
+  class Manager extends Employee {
+    constructor(jobDescription, name, salary, hireDate) {
+        super(name, salary, hireDate);
+        this.jobDescription = jobDescription;
+    }
+    ManagerInfo(){
+        console.log(this.jobDescription+ " by the name of "+this.name+" who makes "+this.salary+" and gained employment "+this.hireDate);
+    }
+  }
+  class Designer extends Manager {
+    constructor(yearsExperience, jobDescription, name, salary, hireDate) {
+        super(jobDescription, name, salary, hireDate);
+        this.yearsExperience = yearsExperience;
+    }
+    DesignerInfo(){
+        console.log(this.yearsExperience+" years of experience as a "+this.jobDescription+". By the name of "+this.name+" who makes"+this.salary+" annually. They started employment on "+this.hireDate);
+    }
+  }
+  class SalesAssociate extends Designer {
+    constructor(degreeCompleted, yearsExperience, jobDescription, name, salary, hireDate) {
+        super(yearsExperience, jobDescription, name, salary, hireDate);
+        this.degreeCompleted = degreeCompleted;
+    }
+    SalesAssociateInfo() {
+        console.log(this.degreeCompleted+" from Portland State University with "+this.yearsExperience+" years of experience. "+this.jobDescription+" by the name of "+this.name+" bringing home "+this.salary+" annually. They began employment on "+this.hireDate);
+    }
+  }
+  let manager = new Manager("Manager", "Delaney", 75000, "November 8th, 2020");
+  let designer = new Designer(6, "Designer", "Keaton", 90000, "October 25th, 2016");
+  let salesassociate = new SalesAssociate("Masters in Business", 33, "Sales Associate", "Kesley", 100000, "January 13th, 1990");
+  Manager.ManagerInfo();
+  Designer.DesignerInfo();
+  SalesAssociate.SalesAssociateInfo();
